0d6c71c476699e66063beaa6cd1cab07faa95b09
3DRX <kjy20030228@icloud.com>
2024-01-16 14:51:22 +0800

    Finish parsing prefix operators, TODO Infix Operators

--- parser/parser.go
+++ parser/parser.go
@@ -16,7 +16,7 @@ const (
 	LESSGREATER
 	SUM
 	PRODUCT
-	PREFIC
+	PREFIX
 	CALL
 )
 
@@ -39,6 +39,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	// read 2 tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
@@ -146,9 +148,14 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	p.errors = append(p.errors, fmt.Sprintf("no prefix parse function for %s found", t))
+}
+
 func (p *Parser) parseExpression(precdence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	return prefix()
@@ -164,4 +171,14 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 	}
 	lit.Value = value
 	return lit
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+	p.nextToken()
+	expression.Right = p.parseExpression(PREFIX)
+	return expression
 }
