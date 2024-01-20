613242b34eb6581a3d8957fa2d0c79ef6e52419c
3DRX <kjy20030228@icloud.com>
2024-01-20 19:48:47 +0800

    Update repl, finish parser.

--- repl/repl.go
+++ repl/repl.go
@@ -3,9 +3,10 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"github.com/3DRX/monkey/lexer"
-	"github.com/3DRX/monkey/token"
 	"io"
+
+	"github.com/3DRX/monkey/lexer"
+	"github.com/3DRX/monkey/parser"
 )
 
 const PROMPT = "$> "
@@ -20,8 +21,19 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
