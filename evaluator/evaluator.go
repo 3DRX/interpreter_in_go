0b5d264d4c73eb61f768cea4fbb811a7dc6446f9
3DRX <kjy20030228@icloud.com>
2024-01-23 11:57:15 +0800

    Add evaluator of integer and boolean literals

---
+++ evaluator/evaluator.go
@@ -0,0 +1,38 @@
+package evaluator
+
+import (
+	"github.com/3DRX/monkey/ast"
+	"github.com/3DRX/monkey/object"
+)
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		if node.Value {
+			return TRUE
+		} else {
+			return FALSE
+		}
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
