0b191abdd4357a17a9cb0a1cecfcb423bdce44d4
3DRX <kjy20030228@icloud.com>
2024-01-24 17:29:15 +0800

    Add environment.go

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/3DRX/monkey/evaluator"
 	"github.com/3DRX/monkey/lexer"
+	"github.com/3DRX/monkey/object"
 	"github.com/3DRX/monkey/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = "$> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -28,7 +30,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
