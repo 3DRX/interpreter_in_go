63b86005d3d9a4d5cf870eec05c5e259c7fa1f76
3DRX <kjy20030228@icloud.com>
2024-01-25 13:13:56 +0800

    Add functions for Array

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -11,9 +11,27 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, expected=2", len(args))
+			}
+			if args[0].Type() != object.ARRAYOBJ {
+				return newError("argument to `push` must be Array, got %s", args[0].Type())
+			}
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+			return &object.Array{Elements: newElements}
 		},
 	},
 }
